efe76861ccce2eaee6281aaba6cab35cb2504929
deepknwldg <raaafik95@gmail.com>
2024-03-24 15:32:42 +0300

    alias package

--- 02.2-basics/main.go
+++ 02.2-basics/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"go-learning/02.2-basics/example"
+	alias "go-learning/02.2-basics/example2"
 )
 
 func main() {
 	fmt.Println(example.MyExportedValue)
+	fmt.Println(alias.MyOtherValue)
 }
