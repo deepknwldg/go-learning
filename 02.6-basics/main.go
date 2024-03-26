d171a22328c36c0ec4ce642abfb280b734294df4
deepknwldg <raaafik95@gmail.com>
2024-03-26 20:02:41 +0300

    slice

--- 02.6-basics/main.go
+++ 02.6-basics/main.go
@@ -53,4 +53,50 @@ func main() {
 	b1[0] = "One!!!"
 	fmt.Println(a1)
 	fmt.Println(b1)
+
+	// slices
+	var zeroSlice []int
+	fmt.Println(zeroSlice, zeroSlice == nil, len(zeroSlice)) // [] true 0
+
+	var oneElementSlice = make([]int, 1, 10)
+	fmt.Println(oneElementSlice, len(oneElementSlice), cap(oneElementSlice)) // [0] 1 10
+
+	sliceWithLenEqCap := make([]int, 5)
+	fmt.Println(sliceWithLenEqCap, len(sliceWithLenEqCap), cap(sliceWithLenEqCap)) // [0 0 0 0 0] 5 5
+
+	myFavSlice := []string{"I", "like", "learning", "Go"}
+	fmt.Println(myFavSlice)
+
+	fmt.Println(myFavSlice[1:2]) // like
+	fmt.Println(myFavSlice[:3])  // I like learning
+
+	myArr := [5]int{20, 15, 5, 30, 25}
+	mySlice := myArr[1:4]
+
+	fmt.Printf("Array: %v, Length: %d, Capacity: %d\n", myArr, len(myArr), cap(myArr))
+	fmt.Printf("Slice: %v, Length: %d, Capacity: %d\n", mySlice, len(mySlice), cap(mySlice))
+
+	for _, v := range myFavSlice {
+		fmt.Println(v)
+	}
+
+	s1 := []string{"a", "b", "c", "d"}
+	s2 := make([]string, len(s1))
+
+	e := copy(s2, s1)
+
+	fmt.Println("Src:", s1)
+	fmt.Println("Dst:", s2)
+	fmt.Println("Elements:", e)
+
+	s3 := make([]string, 2)
+	e2 := copy(s3, s1)
+
+	fmt.Println(s3, e2)
+
+	s3 = append(s3, "c")
+	fmt.Println(s3)
+
+	s3 = append(s3, "d", "e")
+	fmt.Println(s3)
 }
