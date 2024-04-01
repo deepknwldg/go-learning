6743874b9f984c9b8ba168b53391468b8a869f3d
deepknwldg <raaafik95@gmail.com>
2024-04-01 17:40:47 +0300

    reverse

---
+++ tasks/3.4.tasks/go.mod
@@ -0,0 +1,3 @@
+module go-learning/tasks/3.4.tasks
+
+go 1.22.1
---
+++ tasks/3.4.tasks/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"go-learning/tasks/3.4.tasks/task1"
+)
+
+func main() {
+	fmt.Println(task1.Reverse([]int{1, 2, 3, 4}))
+}
