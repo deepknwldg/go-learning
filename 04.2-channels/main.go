8326c9c6e8f8113091841d60c97dea443950c6ff
deepknwldg <raaafik95@gmail.com>
2024-04-09 22:06:15 +0300

    directed channels: for send, for receive. for and forr from chanels

--- 04.2-channels/main.go
+++ 04.2-channels/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func echo(arg string, ch chan string) {
 	ch <- arg // Send
@@ -17,8 +19,7 @@ func main() {
 
 	go echo("Hello World", chInit)
 
-	data := <-chInit // Receive  // то здесь чтение из канала блокируется, до тех пор пока в канал что-нибудь не запишется
-
+	data := <-chInit  // Receive  // то здесь чтение из канала блокируется, до тех пор пока в канал что-нибудь не запишется
 	fmt.Println(data) // Hello World
 
 	// buffered channels
@@ -31,4 +32,46 @@ func main() {
 	fmt.Println(val1, val2)
 
 	close(ch1)
+
+	var c = make(chan int, 3)
+	c <- 20
+	c <- 10
+	c <- 0
+	close(c)
+	// здесь читается больше 3 значений, поэтому последние два будут zero value
+	for i := 0; i < 5; i++ {
+		v, ok := <-c
+		fmt.Printf("closed?: %v val: %d\n", !ok, v)
+	}
+
+	// send to nil channel blocks forever
+	// var c chan string
+	// c <- "Hello world!" // Panic: all goroutines are asleep - deadlock!
+
+	// receive from nil channel blocks forever
+	// var c chan string
+	// fmt.Println(<-c) // Panic: all goroutines are asleep - deadlock!
+
+	// sent to closed channel -> panic
+	// c <- "Hello World!"
+	// close(c)
+	// c <- "Hellow, Panic" // Panic: send on closed channel
+
+	var c2 = make(chan int, 3)
+	c2 <- 20
+	c2 <- 10
+	c2 <- 0
+	close(c2)
+
+	for v := range c2 {
+		fmt.Printf("value %d\n", v)
+	}
+}
+
+func directedEcho(arg string, ch chan<- string) {
+	ch <- arg // Send only
+}
+
+func directedReceive(ch <-chan string) {
+	<-ch
 }
