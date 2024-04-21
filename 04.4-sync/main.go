21a9aa7434ceda037f8244e4d7ec53b58d3f1740
deepknwldg <raaafik95@gmail.com>
2024-04-21 16:45:31 +0300

    try race run

--- 04.4-sync/main.go
+++ 04.4-sync/main.go
@@ -134,3 +134,18 @@ func syncMapExample() {
 
 	wg.Wait()
 }
+
+// go run -race main.go
+func raceExample() {
+	c := make(chan bool)
+	m := make(map[string]string)
+	go func() {
+		m["1"] = "a"
+		c <- true
+	}()
+	m["2"] = "b"
+	<-c
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
+}
